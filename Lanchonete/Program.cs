5e47148958aafe8178beb49a33d8943e1c4629ea
Tony <antony.branquinho@gmail.com>
2023-11-30 09:00:57 -0300

    Esse deu um pouco de trabalho

---
+++ Lanchonete/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lanchonete {
+    class Program {
+        static void Main(string[] args) {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int codigo;
+            double total, preco, quantidade;
+
+            Console.Write("Codigo do produto comprado: ");
+            codigo = int.Parse(Console.ReadLine());
+            Console.Write("Quantidade comprada: ");
+            quantidade = double.Parse(Console.ReadLine());
+
+            //primeiro = 5.00;
+            ///segundo = 3.50;
+            //terceiro = 4.80;
+            //quarto = 8.90;
+            //quinto = 7.32;
+            preco = 0;
+
+            if (codigo == 1) {
+                preco = 5.00;
+            }
+            else if (codigo == 2) {
+                preco = 3.50;
+            }
+            else if (codigo == 3) {
+                preco = 4.80;
+            }
+            else if (codigo == 4) {
+                preco = 8.90;
+            }
+            else if (codigo == 5) {
+                preco = 7.32;
+            }
+
+            total = preco * quantidade;
+
+            Console.WriteLine("Valor a pagar: " + total.ToString("F2"), CI);
+
+
+
+        }
+    }
+}
